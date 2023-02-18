2fa2beef3c018312f28af1241fbf874c4d6c02b7
Joemon V Velassery <joemonv97@gmail.com>
2023-02-19 00:49:37 +0530

    Backend code updated

--- backend/settings.py
+++ backend/settings.py
@@ -6,9 +6,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-0ef*t788x0xgkyaijj@3#6)i(m_$mqqeemsa9e*b=9jbw73_&*"
 
